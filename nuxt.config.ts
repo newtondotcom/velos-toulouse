c30dd4fb8c96a3ab0f9bb57be10ca1d96a09ae67
newtondotcom <asphalt9fr@gmail.com>
2024-04-07 23:23:58 +0200

    miscs forn stations page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 export default defineNuxtConfig({
   //'@nuxtjs/supabase'
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui'],  
+  runtimeConfig: {
+    public : {
+      JC_DECAUX_API_KEY : process.env.JC_DECAUX_API_KEY,
+    },
+  },
+  security: {
+    corsHandler: {
+      origin : '*',
+      methods : ['GET','POST','PUT','DELETE'],
+      allowedHeaders : '*',
+      exposeHeaders : '*',
+    },
+  },
+  ssr: false,
 })
