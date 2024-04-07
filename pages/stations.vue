364a1ecb6fb52e39ad734e7b47f68ad1c64b7aef
newtondotcom <asphalt9fr@gmail.com>
2024-04-07 23:23:44 +0200

    stations page

--- pages/stations.vue
+++ pages/stations.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+const config = useRuntimeConfig();
+const jcdc_key = config.public.JC_DECAUX_API_KEY;
+
+let BikeStations = ref([]);
+let items = ref([]);
+
+/*
 const items = [{
   label: 'Getting Started',
   icon: 'i-heroicons-information-circle',
-  defaultOpen: false,
+  defaultOpen: true,
   content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.'
 }, {
   label: 'Installation',
@@ -26,9 +35,52 @@ const items = [{
   icon: 'i-heroicons-wrench-screwdriver',
   content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.'
 }]
+*/
+
+onMounted(async () => {
+const { data } = await useFetch('https://api.jcdecaux.com/vls/v1/stations', {
+    query: {
+            apiKey: jcdc_key,
+            contract: 'Toulouse'
+        }
+    })
+for (let i = 0; i < data.value.length; i++) {
+    BikeStations.value.push(data.value[i]);
+}
+for (let i = 0; i < BikeStations.value.length; i++) {
+  const bikeStation = BikeStations.value[i];
+  const address = bikeStation.address;
+  const availableBikes = bikeStation.available_bikes;
+  const availableBikeStands = bikeStation.available_bike_stands;
+  const banking = bikeStation.banking;
+  const bonus = bikeStation.bonus;
+  const bikeStands = bikeStation.bike_stands;
+  const last_update = bikeStation.last_update;
+  const name = bikeStation.name.split('- ')[1];
+  const position = bikeStation.position;
+  const deltaTime = Math.floor((Date.now() - last_update) / 1000 / 60);
+    items.value.push({
+        label: name,
+        icon: 'i-heroicons-information-circle',
+        content: bikeStation,
+        availableBikeStands: availableBikeStands,
+        availableBikes: availableBikes,
+        deltaTime: deltaTime,
+    })
+}})
+
 </script>
 
 <template>
-  <UAccordion :items="items" />
+  <UAccordion :items="items">
+  <template #item="{ item }">
+    {{ item.content }}
+      <p class="italic text-gray-900 dark:text-white text-center">
+        {{ item.availableBikeStands }}
+        {{item.availableBikes}}
+        {{item.deltaTime}}
+      </p>
+    </template>
+  </UAccordion>
 </template>
 
