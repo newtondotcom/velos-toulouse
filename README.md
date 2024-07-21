d0587cb2e36cecd4f82b4a48fa2b7e47e4cf8c25
newtondotcom <asphalt9fr@gmail.com>
2024-07-21 15:06:30 +0200

    feat : more reliable way to get station name for other contracts

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 > VelosToulouse is a PWA so you can add it to your home screen and get a greater ux.
 
-Since this app is using JCDecaux API, you should be able to fork it and publish it for [one of those cities](https://developer.jcdecaux.com/#/gbfs?page=usage) by changing the contract name in [here](/src/lib/fetch.ts).
+Since this app is using JCDecaux API, you should be able to fork it and publish it for [one of those cities](https://developer.jcdecaux.com/#/gbfs?page=usage) by changing the contract name in [here](/src/lib/fetch.ts) and changing default location in [here](/src/components/vt/Bikes.vue).
--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -4,6 +4,7 @@ import type { IStation, IStationAPI, IFavorites } from '@/component/lib/types';
 
 const jcdc_key = import.meta.env.VITE_JC_DECAUX_API_KEY;
 const maxNameLength = 25;
+const contract_name = 'toulouse';
 
 function calculatedColor(percentage: number) {
   const red = 255 - Math.round(percentage * 255);
@@ -16,94 +17,10 @@ export async function fetchBikeStations(latitude: number, longitude: number) {
   const data: IStationAPI[] = await ofetch('https://api.jcdecaux.com/vls/v3/stations', {
     query: {
       apiKey: jcdc_key,
-      contract: 'Toulouse',
+      contract: contract_name,
     },
     credentials: 'include',
   });
-  /*
-  const data: IStationAPI[] = [
-    {
-      number: 55,
-      contractName: 'toulouse',
-      name: '00055 - SAINT-SERNIN - G. ARNOULT',
-      address: '2 RUE GATIEN ARNOULT',
-      position: {
-        latitude: 43.609022,
-        longitude: 1.441105,
-      },
-      banking: false,
-      bonus: false,
-      status: 'OPEN',
-      lastUpdate: '2024-07-15T13:50:21Z',
-      connected: true,
-      overflow: false,
-      shape: null,
-      totalStands: {
-        availabilities: {
-          bikes: 1,
-          stands: 14,
-          mechanicalBikes: 1,
-          electricalBikes: 0,
-          electricalInternalBatteryBikes: 0,
-          electricalRemovableBatteryBikes: 0,
-        },
-        capacity: 15,
-      },
-      mainStands: {
-        availabilities: {
-          bikes: 1,
-          stands: 14,
-          mechanicalBikes: 1,
-          electricalBikes: 0,
-          electricalInternalBatteryBikes: 0,
-          electricalRemovableBatteryBikes: 0,
-        },
-        capacity: 15,
-      },
-      overflowStands: null,
-    },
-    {
-      number: 80,
-      contractName: 'toulouse',
-      name: '00055 - SAINT-SERNIN - G. ARNOULT',
-      address: '2 RUE GATIEN ARNOULT',
-      position: {
-        latitude: 43.609022,
-        longitude: 1.441105,
-      },
-      banking: false,
-      bonus: false,
-      status: 'OPEN',
-      lastUpdate: '2024-07-15T13:50:21Z',
-      connected: true,
-      overflow: false,
-      shape: null,
-      totalStands: {
-        availabilities: {
-          bikes: 1,
-          stands: 14,
-          mechanicalBikes: 1,
-          electricalBikes: 0,
-          electricalInternalBatteryBikes: 0,
-          electricalRemovableBatteryBikes: 0,
-        },
-        capacity: 15,
-      },
-      mainStands: {
-        availabilities: {
-          bikes: 1,
-          stands: 14,
-          mechanicalBikes: 1,
-          electricalBikes: 0,
-          electricalInternalBatteryBikes: 0,
-          electricalRemovableBatteryBikes: 0,
-        },
-        capacity: 15,
-      },
-      overflowStands: null,
-    },
-  ];
-  */
 
   const favorites: IFavorites[] = JSON.parse(localStorage.getItem('favorites') || '[]');
 
@@ -115,7 +32,8 @@ export async function fetchBikeStations(latitude: number, longitude: number) {
     );
     const formattedDistance: Number = parseFloat(distance).toFixed(0);
 
-    let label = bikeStation.name.split('- ')[1];
+    const index = bikeStation.name.indexOf('-');
+    let label = bikeStation.name.slice(index + 1).trim();
     if (label.length > maxNameLength) {
       label = label.slice(0, maxNameLength - 3) + '...';
     }
